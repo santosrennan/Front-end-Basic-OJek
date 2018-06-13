1f26424e7d24a98b85c02fab15e2da5cd6158982
santosrennan <40223602+santosrennan@users.noreply.github.com>
2018-06-13 00:48:02 -0300

    Prototipo basic no justmind
    Simples para ter uma noção bem superficial.

---
+++ Prototipo App justmind/resources/screens/cadcdbd5-d639-4865-b840-0c142b724760-1480879007230.js
@@ -0,0 +1,536 @@
+jQuery("#simulation")
+  .on("click", ".s-cadcdbd5-d639-4865-b840-0c142b724760 .click", function(event, data) {
+    var jEvent, jFirer, cases;
+    if(data === undefined) { data = event; }
+    jEvent = jimEvent(event);
+    jFirer = jEvent.getEventFirer();
+    if(jFirer.is("#s-Ellipse_13")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimShow",
+                  "parameter": {
+                    "target": "#s-Ellipse_14",
+                    "effect": {
+                      "type": "fade",
+                      "easing": "linear",
+                      "duration": 200
+                    }
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                },
+                {
+                  "action": "jimHide",
+                  "parameter": {
+                    "target": "#s-Ellipse_14",
+                    "effect": {
+                      "type": "fade",
+                      "easing": "linear",
+                      "duration": 200
+                    }
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-checkbox-on-light_4")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimSetValue",
+                  "parameter": {
+                    "target": "#s-checkbox-on-light_4",
+                    "value": null
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-checkbox-on-light_6")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimSetValue",
+                  "parameter": {
+                    "target": "#s-checkbox-on-light_6",
+                    "value": ""
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        },
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimSetValue",
+                  "parameter": {
+                    "target": "#s-checkbox-on-light_6",
+                    "value": null
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Button_1")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/a1c99e62-0c4c-47f8-92f0-86289a43aa0e"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Label_32")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimShow",
+                  "parameter": {
+                    "target": "#s-Dynamic_Panel_1",
+                    "effect": {
+                      "type": "fade",
+                      "duration": 100
+                    }
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        },
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimMove",
+                  "parameter": {
+                    "target": "#s-Dynamic_Panel_1",
+                    "top": {
+                      "type": "movetoposition",
+                      "value": "0"
+                    },
+                    "left": {
+                      "type": "movetoposition",
+                      "value": "306"
+                    },
+                    "containment": false,
+                    "effect": {
+                      "type": "none",
+                      "easing": "easeInQuad",
+                      "duration": 300
+                    }
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Paragraph_1")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimMove",
+                  "parameter": {
+                    "target": "#s-Dynamic_Panel_1",
+                    "top": {
+                      "type": "movetoposition",
+                      "value": "0"
+                    },
+                    "left": {
+                      "type": "movetoposition",
+                      "value": "0"
+                    },
+                    "containment": false,
+                    "effect": {
+                      "type": "none",
+                      "easing": "easeInQuad",
+                      "duration": 300
+                    }
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                },
+                {
+                  "action": "jimHide",
+                  "parameter": {
+                    "target": "#s-Paragraph_1",
+                    "effect": {
+                      "type": "fade",
+                      "easing": "easeInQuad",
+                      "duration": 100
+                    }
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Panel_1")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimShow",
+                  "parameter": {
+                    "target": "#s-Label_32",
+                    "effect": {
+                      "type": "fade",
+                      "easing": "easeOutQuad",
+                      "duration": 200
+                    }
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        },
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimMove",
+                  "parameter": {
+                    "target": "#s-Dynamic_Panel_1",
+                    "top": {
+                      "type": "movetoposition",
+                      "value": "0"
+                    },
+                    "left": {
+                      "type": "movetoposition",
+                      "value": "305"
+                    },
+                    "containment": false,
+                    "effect": {
+                      "type": "none",
+                      "easing": "easeInQuad",
+                      "duration": 300
+                    }
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "timed",
+          "delay": 300
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Image_1")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimMove",
+                  "parameter": {
+                    "target": "#s-Dynamic_Panel_1",
+                    "top": {
+                      "type": "movetoposition",
+                      "value": "0"
+                    },
+                    "left": {
+                      "type": "movetoposition",
+                      "value": "-306"
+                    },
+                    "containment": false,
+                    "effect": {
+                      "type": "none",
+                      "easing": "easeInQuad",
+                      "duration": 300
+                    }
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                },
+                {
+                  "action": "jimShow",
+                  "parameter": {
+                    "target": "#s-Dynamic_Panel_1",
+                    "effect": {
+                      "type": "fade",
+                      "easing": "easeOutQuad",
+                      "duration": 100
+                    }
+                  },
+                  "exectype": "timed",
+                  "delay": 200
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Image_2")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimMove",
+                  "parameter": {
+                    "target": "#s-Dynamic_Panel_1",
+                    "top": {
+                      "type": "movetoposition",
+                      "value": "0"
+                    },
+                    "left": {
+                      "type": "movetoposition",
+                      "value": "-306"
+                    },
+                    "containment": false,
+                    "effect": {
+                      "type": "none",
+                      "easing": "easeInQuad",
+                      "duration": 300
+                    }
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                },
+                {
+                  "action": "jimHide",
+                  "parameter": {
+                    "target": "#s-Paragraph_1",
+                    "effect": {
+                      "type": "fade",
+                      "duration": 100
+                    }
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    }
+  })
+  .on("click", ".s-cadcdbd5-d639-4865-b840-0c142b724760 .toggle", function(event, data) {
+    var jEvent, jFirer, cases;
+    if(data === undefined) { data = event; }
+    jEvent = jimEvent(event);
+    jFirer = jEvent.getEventFirer();
+    if(jFirer.is("#s-checkbox-on-light_4")) {
+      if(jFirer.data("jimHasToggle")) {
+        jFirer.removeData("jimHasToggle");
+        jEvent.undoCases(jFirer);
+      } else {
+        jFirer.data("jimHasToggle", true);
+        event.backupState = true;
+        event.target = jFirer;
+        cases = [
+          {
+            "blocks": [
+              {
+                "actions": [
+                  {
+                    "action": "jimSetValue",
+                    "parameter": {
+                      "target": "#s-checkbox-on-light_4",
+                      "value": ""
+                    },
+                    "exectype": "serial",
+                    "delay": 0
+                  },
+                  {
+                    "action": "jimChangeStyle",
+                    "parameter": [ {
+                      "#s-cadcdbd5-d639-4865-b840-0c142b724760 #s-checkbox-on-light_4": {
+                        "attributes": {
+                          "font-size": "20.0pt",
+                          "font-family": "materialdesignjim-Regular,Arial"
+                        }
+                      }
+                    },{
+                      "#s-cadcdbd5-d639-4865-b840-0c142b724760 #s-checkbox-on-light_4 .valign": {
+                        "attributes": {
+                          "vertical-align": "middle",
+                          "text-align": "left"
+                        }
+                      }
+                    },{
+                      "#s-cadcdbd5-d639-4865-b840-0c142b724760 #s-checkbox-on-light_4 span": {
+                        "attributes": {
+                          "color": "#333333",
+                          "text-align": "left",
+                          "text-decoration": "none",
+                          "font-family": "materialdesignjim-Regular,Arial",
+                          "font-size": "20.0pt"
+                        }
+                      }
+                    } ],
+                    "exectype": "serial",
+                    "delay": 0
+                  }
+                ]
+              }
+            ],
+            "exectype": "serial",
+            "delay": 0
+          }
+        ];
+        jEvent.launchCases(cases);
+      }
+    } else if(jFirer.is("#s-checkbox-on-light_6")) {
+      if(jFirer.data("jimHasToggle")) {
+        jFirer.removeData("jimHasToggle");
+        jEvent.undoCases(jFirer);
+      } else {
+        jFirer.data("jimHasToggle", true);
+        event.backupState = true;
+        event.target = jFirer;
+        cases = [
+          {
+            "blocks": [
+              {
+                "actions": [
+                  {
+                    "action": "jimSetValue",
+                    "parameter": {
+                      "target": "#s-checkbox-on-light_6",
+                      "value": ""
+                    },
+                    "exectype": "serial",
+                    "delay": 0
+                  },
+                  {
+                    "action": "jimChangeStyle",
+                    "parameter": [ {
+                      "#s-cadcdbd5-d639-4865-b840-0c142b724760 #s-checkbox-on-light_6": {
+                        "attributes": {
+                          "font-size": "20.0pt",
+                          "font-family": "materialdesignjim-Regular,Arial"
+                        }
+                      }
+                    },{
+                      "#s-cadcdbd5-d639-4865-b840-0c142b724760 #s-checkbox-on-light_6 .valign": {
+                        "attributes": {
+                          "vertical-align": "middle",
+                          "text-align": "left"
+                        }
+                      }
+                    },{
+                      "#s-cadcdbd5-d639-4865-b840-0c142b724760 #s-checkbox-on-light_6 span": {
+                        "attributes": {
+                          "color": "#8AD9D2",
+                          "text-align": "left",
+                          "text-decoration": "none",
+                          "font-family": "materialdesignjim-Regular,Arial",
+                          "font-size": "20.0pt"
+                        }
+                      }
+                    } ],
+                    "exectype": "serial",
+                    "delay": 0
+                  }
+                ]
+              }
+            ],
+            "exectype": "serial",
+            "delay": 0
+          }
+        ];
+        jEvent.launchCases(cases);
+      }
+    }
+  });
---
+++ Prototipo App justmind/resources/scroll-1480879007230.js
@@ -0,0 +1,17 @@
+(function(window, undefined) {
+
+    /*********************** START STATIC ACCESS METHODS ************************/
+
+    jQuery.extend(jimMobile, {
+        "loadScrollBars": function() {
+            jQuery(".s-12f95033-51c0-4a0b-bb64-c80c715ca252 .ui-page").overscroll({ showThumbs:true, direction:'vertical' });
+            jQuery(".s-a1c99e62-0c4c-47f8-92f0-86289a43aa0e .ui-page").overscroll({ showThumbs:true, direction:'vertical' });
+            jQuery(".s-1432c19d-c891-4d09-9d9e-a0e9b91f9494 .ui-page").overscroll({ showThumbs:true, direction:'vertical' });
+            jQuery(".s-cadcdbd5-d639-4865-b840-0c142b724760 .ui-page").overscroll({ showThumbs:true, direction:'vertical' });
+            jQuery(".s-8a4897e9-b064-4cfa-b08e-6f61f2d8ba02 .ui-page").overscroll({ showThumbs:true, direction:'vertical' });
+         }
+    });
+
+    /*********************** END STATIC ACCESS METHODS ************************/
+
+}) (window);
