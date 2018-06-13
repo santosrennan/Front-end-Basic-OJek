cf9930cce2372c69e1cc6a1e455cf1af854f6912
santosrennan <40223602+santosrennan@users.noreply.github.com>
2018-06-13 00:41:55 -0300

    SiteFinal+RotasGoogle
    Versão basica do site final projeto pessoal para  trabalho da faculdade UNIRIO.

---
+++ SiteFinal Jek - Projeto/Prototype2/resources/screens/a1c99e62-0c4c-47f8-92f0-86289a43aa0e-1480877138948.js
@@ -0,0 +1,60 @@
+jQuery("#simulation")
+  .on("click", ".s-a1c99e62-0c4c-47f8-92f0-86289a43aa0e .click", function(event, data) {
+    var jEvent, jFirer, cases;
+    if(data === undefined) { data = event; }
+    jEvent = jimEvent(event);
+    jFirer = jEvent.getEventFirer();
+    if(jFirer.is("#s-Image_26")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/cadcdbd5-d639-4865-b840-0c142b724760"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Label_11")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimChangeStyle",
+                  "parameter": [  ],
+                  "exectype": "serial",
+                  "delay": 0
+                },
+                {
+                  "action": "jimShow",
+                  "parameter": {
+                    "target": "#s-Label_11"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    }
+  });
